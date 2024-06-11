9bcbe8adddeda3cc108cb5af4d4084813f580265
Damian Pietrewicz <d.pietrewicz4@gmail.com>
2024-06-11 17:13:55 +0200

    displayMovements function added

--- script.js
+++ script.js
@@ -27,3 +27,48 @@ const labelDate = document.querySelector('date');
 const labelBalance = document.querySelector('.balance__value');
 const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
+
+const containerMovements = document.querySelector('.movements__container');
+
+const inputDescription = document.querySelector('#description');
+const inputAmount = document.querySelector('#amount');
+
+const btnIncome = document.querySelector('#add__income');
+const btnExpense = document.querySelector('#add__expense');
+
+const displayMovements = function () {
+   const description = inputDescription.value;
+   const amount = inputAmount.value;
+   const now = new Date();
+   const locale = navigator.language;
+   const options = {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+   };
+
+   const date = new Intl.DateTimeFormat(locale, options).format(now);
+
+   const html = `<div class="movements__row">
+                  <span class="fa-solid fa-house card__icon"></span>
+                  <span class="movements__row--name">
+                     <label class="movements__row__name--label"
+                        >${description}</label
+                     >
+                     <div class="movements__type movements__type--income">
+                        Transportation
+                     </div>
+                  </span>
+                  <div class="movements__date">${date}</div>
+                  <div class="movements__value">${amount}€</div>
+               </div>`;
+
+   containerMovements.insertAdjacentHTML('afterbegin', html);
+};
+
+// displayMovements();
+
+btnIncome.addEventListener('click', function (e) {
+   e.preventDefault();
+   displayMovements();
+});
