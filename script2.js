e0aad4a20c0d238701496dad0583e0c9471dccdf
Damian Pietrewicz <d.pietrewicz4@gmail.com>
2024-07-12 14:57:01 +0200

    card's amount updates

--- script2.js
+++ script2.js
@@ -1,17 +1,5 @@
 'use strict';
 
-const account = {
-   id: [111, 222, 333],
-   description: ['apple', 'orange', 'watermelon'],
-   amount: [1, 2, 3],
-   category: ['Groceries', 'Transportation', 'Housing'],
-   date: [
-      '2019-11-01T13:15:33.035Z',
-      '2019-11-30T09:48:16.867Z',
-      '2019-12-25T06:04:23.907Z',
-   ],
-};
-
 const incomeBtn = document.querySelector('#add__income');
 const expenseBtn = document.querySelector('#add__expense');
 
@@ -45,8 +33,6 @@ let incomes = [
    },
 ];
 
-// let housing = [];
-
 incomeBtn.addEventListener('click', function (e) {
    e.preventDefault();
 
@@ -54,18 +40,9 @@ incomeBtn.addEventListener('click', function (e) {
 
    incomes = [...incomes, newIncome];
 
+   itemsContainer.innerHTML = '';
+
    const ui = new UI();
-
-   console.log(incomes);
-
-   // const desc = document.querySelector('#description').value;
-   // console.log(desc);
-   // account.description.push(desc);
-   // console.log(account);
-
-   // const newArr = account.amount.map((item) => item);
-   // console.log(newArr);
-   // newIncome.clearInputs();
 });
 
 // ITEM CONTROLLER
@@ -80,7 +57,6 @@ class Item {
       this.getInputValues();
       this.capitalize();
       this.addDate();
-      // this.addValues();
    }
 
    createId() {
@@ -105,14 +81,6 @@ class Item {
       // this.date = new Date().toISOString();
    }
 
-   // addValues() {
-   //    incomes.id.push(this.id);
-   //    incomes.description.push(this.description);
-   //    incomes.amount.push(+this.amount);
-   //    incomes.category.push(this.category);
-   //    incomes.date.push(this.date);
-   // }
-
    clearInputs() {
       this.description = document.querySelector('#description').value = '';
       this.amount = document.querySelector('#amount').value = '';
@@ -126,8 +94,10 @@ class UI {
    constructor() {
       this.displayData();
       this.updateIn();
-      this.updateBudgets('Groceries');
+      this.updateBudgets('Transportation');
       this.updateBudgets('Housing');
+      this.updateBudgets('Groceries');
+      this.updateBudgets('Entertainment');
    }
 
    displayData() {
@@ -165,34 +135,14 @@ class UI {
    }
 
    updateBudgets(cat) {
-      console.log(`Hello '${cat}'`);
       const filterCat = incomes.filter((item) => item.category === `${cat}`);
       const catCount = filterCat.map((item) => +item.amount);
       const catSum = catCount.reduce((a, b) => a + b, 0);
 
-      const lower = cat.toLowerCase();
-      const displayCat = (document.querySelector(`#${lower}`).innerHTML =
+      const lowerCase = cat.toLowerCase();
+      const displayCat = (document.querySelector(`#${lowerCase}`).innerHTML =
          catSum.toFixed(2));
-      console.log(filterCat);
-      console.log(catCount);
    }
 }
 
 const app = new UI();
-// [...account.description].forEach(function (child) {
-//    // console.log(child);
-// });
-
-// Array.from(account).forEach((item) => console.log(item));
-
-// const arr3 = Object.keys(account).map(function (key) {
-//    return account[key];
-// });
-// console.log(arr3);
-
-// const arr4 = Object.entries(account);
-// // console.log(arr4);
-
-// for (let el in account) {
-//    console.log(`On ${el}, it's ${account[el]}!`);
-// }
