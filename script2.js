dcf35776a423a85960c712ffd04c524dc5233c04
Damian Pietrewicz <d.pietrewicz4@gmail.com>
2024-07-22 14:16:32 +0200

    formatCur(), updateBalance() added

--- script2.js
+++ script2.js
@@ -1,151 +1,158 @@
-"use strict";
+'use strict';
 
-const incomeBtn = document.querySelector("#add__income");
-const expenseBtn = document.querySelector("#add__expense");
+// 1. Implement SORTING
+// 2. Add type of movement : income || expense
 
-const description = document.querySelector("#description");
-const amount = document.querySelector("#amount");
+const incomeBtn = document.querySelector('#add__income');
+const expenseBtn = document.querySelector('#add__expense');
 
-const inputForm = document.querySelector("#input__form");
-const type = document.querySelector(".select__type");
-const moneyAvailable = document.querySelector("#amount__available");
-const moneyIn = document.querySelector("#amount__in");
-const moneyOut = document.querySelector("#amount__out");
-const itemsContainer = document.querySelector("#items__container");
+const description = document.querySelector('#description');
+const amount = document.querySelector('#amount');
 
-const inputType = document.querySelector(".form__input--type");
-const inn = document.querySelector(".inputs__container--type");
+const inputForm = document.querySelector('#input__form');
+const type = document.querySelector('.select__type');
+const moneyBalance = document.querySelector('#amount__balance');
+const dateBalance = document.querySelector('#date__balance');
+const moneyIn = document.querySelector('#amount__in');
+const moneyOut = document.querySelector('#amount__out');
+const itemsContainer = document.querySelector('#items__container');
+
+const inputType = document.querySelector('.form__input--type');
+const inn = document.querySelector('.inputs__container--type');
 
 let incomes = [
-    {
-        id: 3600,
-        description: "selling",
-        amount: "1000",
-        category: "Groceries",
-        date: "11/07/2024",
-    },
-    {
-        id: 3300,
-        description: "salary",
-        amount: "1000",
-        category: "Housing",
-        date: "21/07/2024",
-    },
+   {
+      id: 3600,
+      description: 'selling',
+      amount: '1000',
+      category: 'Groceries',
+      date: '11/07/2024',
+   },
+   {
+      id: 3300,
+      description: 'salary',
+      amount: '1000',
+      category: 'Housing',
+      date: '21/07/2024',
+   },
 ];
 
 let expenses = [
-    {
-        id: 4300,
-        description: "rent",
-        amount: "800",
-        category: "Housing",
-        date: "21/07/2024",
-    },
+   {
+      id: 4300,
+      description: 'rent',
+      amount: '800',
+      category: 'Housing',
+      date: '21/07/2024',
+   },
 ];
 
-incomeBtn.addEventListener("click", function (e) {
-    e.preventDefault();
+incomeBtn.addEventListener('click', function (e) {
+   e.preventDefault();
 
-    const newIncome = new Item();
-    newIncome.type = "income";
+   const newIncome = new Item();
+   newIncome.type = 'income';
 
-    incomes = [...incomes, newIncome];
+   incomes = [...incomes, newIncome];
 
-    itemsContainer.innerHTML = "";
+   itemsContainer.innerHTML = '';
 
-    const ui = new UI();
-    // ui.displayData(transactions);
+   const ui = new UI();
+   // ui.displayData(transactions);
 
-    Item.clearInputs();
-    console.log(incomes);
+   Item.clearInputs();
+   console.log(incomes);
 });
 
-expenseBtn.addEventListener("click", function (e) {
-    e.preventDefault();
+expenseBtn.addEventListener('click', function (e) {
+   e.preventDefault();
 
-    const newExpense = new Item();
-    newExpense.type = "expense";
+   const newExpense = new Item();
+   newExpense.type = 'expense';
 
-    expenses = [...expenses, newExpense];
+   expenses = [...expenses, newExpense];
 
-    itemsContainer.innerHTML = "";
+   itemsContainer.innerHTML = '';
 
-    const ui = new UI();
-    // ui.displayData(transactions);
+   const ui = new UI();
+   // ui.displayData(transactions);
 
-    Item.clearInputs();
-    console.log(expenses);
+   Item.clearInputs();
+   console.log(expenses);
 });
 
 // ITEM CONTROLLER
 class Item {
-    constructor(id, description, amount, category, date) {
-        this.id = id;
-        this.description = description;
-        this.amount = amount;
-        this.category = category;
-        this.date = date;
-        this.validateInputValues();
-    }
+   constructor(id, description, amount, category, date) {
+      this.id = id;
+      this.description = description;
+      this.amount = amount;
+      this.category = category;
+      this.date = date;
+      this.validateInputValues();
+   }
 
-    validateInputValues() {
-        if (description.value && amount.value !== "") {
-            this.getInputValues();
-            this.createId();
-            this.capitalize();
-            this.addDate();
-        } else {
-            return alert("Please fill in values");
-        }
-    }
+   validateInputValues() {
+      if (description.value && amount.value !== '') {
+         this.getInputValues();
+         this.createId();
+         this.capitalize();
+         this.addDate();
+      } else {
+         return alert('Please fill in values');
+      }
+   }
 
-    getInputValues() {
-        this.description = description.value;
-        this.amount = amount.value;
-        this.category = document.querySelector(
-            'input[name="category"]:checked'
-        ).value;
-    }
+   getInputValues() {
+      this.description = description.value;
+      this.amount = amount.value;
+      this.category = document.querySelector(
+         'input[name="category"]:checked'
+      ).value;
+   }
 
-    createId() {
-        this.id = Math.floor(Math.random() * 10000);
-    }
+   createId() {
+      this.id = Math.floor(Math.random() * 10000);
+   }
 
-    capitalize() {
-        this.category = this.category[0].toUpperCase() + this.category.slice(1);
-        return this.category;
-    }
+   capitalize() {
+      this.category = this.category[0].toUpperCase() + this.category.slice(1);
+      return this.category;
+   }
 
-    addDate() {
-        this.date = new Intl.DateTimeFormat("pt-PT").format(this.date);
-        // this.date = new Date().toISOString();
-    }
+   addDate() {
+      this.date = new Intl.DateTimeFormat('pt-PT').format(this.date);
+      // this.date = new Date().toISOString();
+   }
 
-    static clearInputs() {
-        this.description = document.querySelector("#description").value = "";
-        this.amount = document.querySelector("#amount").value = "";
-        // this.category = document.querySelector(
-        //    'input[name="category"]:checked'
-        // ).checked = false;
-    }
+   static clearInputs() {
+      this.description = document.querySelector('#description').value = '';
+      this.amount = document.querySelector('#amount').value = '';
+      // this.category = document.querySelector(
+      //    'input[name="category"]:checked'
+      // ).checked = false;
+   }
 }
 
 class UI {
-    constructor() {
-        this.displayData();
-        this.updateIn();
-        this.updateOut();
-        this.updateBudgets("Transportation");
-        this.updateBudgets("Housing");
-        this.updateBudgets("Groceries");
-        this.updateBudgets("Entertainment");
-    }
+   constructor() {
+      this.displayData();
+      this.updateIn();
+      this.updateOut();
+      this.updateBudgets('Transportation');
+      this.updateBudgets('Housing');
+      this.updateBudgets('Groceries');
+      this.updateBudgets('Entertainment');
+      this.updateBalance();
+      this.sortData();
+   }
 
-    displayData() {
-        const transactions = [...incomes, ...expenses];
-        //   console.log(transactions);
-        transactions.forEach((item) => {
-            const html = `<div class="movements__row">
+   displayData() {
+      const transactions = [...incomes, ...expenses];
+      console.log(transactions);
+      transactions.forEach((item) => {
+         const formattedMov = this.formatCur(item.amount);
+         const html = `<div class="movements__row">
                      <span class="fa-solid fa-house card__icon"></span>
                      <span class="movements__row--description">
                         <label class="movements__row__description--name"
@@ -156,63 +163,93 @@ class UI {
                         </div>
                      </span>
                      <div class="movements__date">${item.date}</div>
-                     <div class="movements__value">${item.amount}</div>
-                     <p class="symbol">€</p>
+                     <div class="movements__value">${formattedMov}</div>
                       </div>`;
 
-            itemsContainer.insertAdjacentHTML("afterbegin", html);
-        });
-    }
+         itemsContainer.insertAdjacentHTML('afterbegin', html);
+      });
+   }
 
-    // In sum of last transactions
-    updateIn() {
-        // const allIncome = document.querySelectorAll('.movements__value');
-        const incomeCount = incomes.map((item) => +item.amount);
-        //   console.log(incomeCount);
-        const incomeSum = incomeCount.reduce(function (a, b) {
-            return a + b;
-        }, 0);
+   sortData() {
+      const transactions = [...incomes, ...expenses];
+      const amountArr = transactions.map((item) => +item.amount);
+      console.log(amountArr);
 
-        moneyIn.innerHTML = incomeSum.toFixed(2);
-    }
+      amountArr.sort((a, b) => {
+         if (a > b) return 1;
+         if (a < b) return -1;
+      });
+      console.log(amountArr);
+   }
 
-    // Out sum of last transactions
-    updateOut() {
-        // const allIncome = document.querySelectorAll('.movements__value');
-        const expensesCount = expenses.map((item) => +item.amount);
-        const expensesSum = expensesCount.reduce(function (a, b) {
-            return a + b;
-        }, 0);
+   formatCur(value) {
+      return new Intl.NumberFormat('en-us', {
+         style: 'currency',
+         currency: 'USD',
+      }).format(value);
+   }
 
-        moneyOut.innerHTML = expensesSum.toFixed(2);
-    }
+   // In sum of last transactions
+   updateIn() {
+      // const allIncome = document.querySelectorAll('.movements__value');
+      const incomeCount = incomes.map((item) => +item.amount);
+      //   console.log(incomeCount);
+      const incomeSum = incomeCount.reduce(function (a, b) {
+         return a + b;
+      }, 0);
 
-    // Cards update
-    updateBudgets(cat) {
-        const filterCat = incomes.filter((item) => item.category === `${cat}`);
-        //   console.log(filterCat);
-        const filterCat2 = expenses.filter(
-            (item) => item.category === `${cat}`
-        );
-        //   console.log(filterCat2);
+      moneyIn.innerHTML = this.formatCur(incomeSum);
+   }
 
-        const catCount = filterCat.map((item) => +item.amount);
-        const catCount2 = filterCat2.map((item) => +item.amount);
+   // Out sum of last transactions
+   updateOut() {
+      // const allIncome = document.querySelectorAll('.movements__value');
+      const expensesCount = expenses.map((item) => +item.amount);
+      const expensesSum = expensesCount.reduce(function (a, b) {
+         return a + b;
+      }, 0);
 
-        const catSum = catCount.reduce((a, b) => a + b, 0);
-        const catSum2 = catCount2.reduce((a, b) => a + b, 0);
+      moneyOut.innerHTML = this.formatCur(expensesSum);
+   }
 
-        //   console.log(catSum);
-        //   console.log(catSum2);
+   // Cards update
+   updateBudgets(cat) {
+      const filterIn = incomes.filter((item) => item.category === `${cat}`);
+      //   console.log(filterCat);
+      const filterOut = expenses.filter((item) => item.category === `${cat}`);
+      //   console.log(filterCat2);
 
-        const lowerCase = cat.toLowerCase();
+      const countIn = filterIn.map((item) => +item.amount);
+      const countOut = filterOut.map((item) => +item.amount);
 
-        const total = catSum - catSum2;
-        //   console.log(total);
+      const sumIn = countIn.reduce((a, b) => a + b, 0);
+      const sumOut = countOut.reduce((a, b) => a + b, 0);
 
-        const displayCat = (document.querySelector(`#${lowerCase}`).innerHTML =
-            total.toFixed(2));
-    }
+      //   console.log(catSum);
+      //   console.log(catSum2);
+      const lowerCase = cat.toLowerCase();
+
+      const total = sumIn - sumOut;
+      // console.log(total);
+
+      const displayCat = (document.querySelector(`#${lowerCase}`).innerHTML =
+         this.formatCur(total));
+   }
+
+   updateBalance() {
+      const sumIn = incomes
+         .map((item) => +item.amount)
+         .reduce((a, b) => a + b, 0);
+
+      const sumOut = expenses
+         .map((item) => +item.amount)
+         .reduce((a, b) => a + b, 0);
+
+      const balance = sumIn - sumOut;
+
+      moneyBalance.innerHTML = this.formatCur(balance);
+      dateBalance.innerHTML = new Intl.DateTimeFormat('pt-PT').format();
+   }
 }
 
 const app = new UI();
